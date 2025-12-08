e0db74ac480e85e58ddd3aa3b48a4df64ae6f6ae
itay <itay2318@gmail.com>
2025-12-08 19:34:57 +0200

    fixed the /upload-form to giving "json_data" instead of "form"

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
 const express = require("express");
 const app = express();
-const cors = require("cors");
 const PORT = process.env.PORT || 3000;
 const {createClient} = require("@supabase/supabase-js");
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
 
-app.use(cors());
 app.use(express.json());
 
 async function login(email, password) {
 
-    const {user, session, error} = await supabase.auth.signInWithPassword({
+    const {session, error} = await supabase.auth.signInWithPassword({
         email,
         password,
     });
@@ -26,10 +24,14 @@ async function login(email, password) {
 const USER_TOKEN = login(process.env.USERNAME, process.env.PASSWORD);
 
 app.post('/upload-form', async (req, res) => {
-    const {id, form} = Object.entries(req.body);
+    const {id, json_data} = Object.entries(req.body);
 
     const {error} = await supabase.from("forms")
-        .insert([{id, form}], {headers: {Authorization: `Bearer ${USER_TOKEN}`}}
+        .insert([{id, json_data}], {
+                headers: {
+                    Authorization: `Bearer ${USER_TOKEN}`
+                }
+            }
         );
 
     if (error) {
